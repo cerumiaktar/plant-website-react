dc72866aa9b1331061d1a419ac3f4f3d2420e624
unknown <ce.rumiakter@gmail.com>
2025-01-23 18:23:13 +0600

    add all category

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Category from "../Category/Category";
 
 
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() =>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data =>setCategories(data))
+    } ,[])
     return (
         <div className="container mx-auto">
-            <h1 className="text-2xl font-medium text-center">Category</h1>
+            <h1 className="text-2xl font-medium text-center mb-14">Category</h1>
+            <div className="grid md:grid-cols-4 gap-10">
+                {
+                    categories.map(category => <Category category ={category}></Category>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,9 +1,14 @@
 
 
-const Category = () => {
+const Category = ({ category }) => {
+    const { id, title, img } = category;
     return (
-        <div>
-            
+        <div class="bg-[#E8FFDE] flex justify-between items-center pr-8 h-[116px] rounded-2xl w-[300px]">
+            <img className="w-[114px] mb-14" src={img} alt="" />
+            <div>
+                <h2 class="text-lg font-medium">{title}</h2>
+                <button class="bg-[#23A204] px-2 py-1 rounded mt-2 text-white">Shop Now</button>
+            </div>
         </div>
     );
 };
