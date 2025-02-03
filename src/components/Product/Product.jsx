8c183d9bc74f773acb530e87ba9f1c70dd2080e2
unknown <ce.rumiakter@gmail.com>
2025-02-03 18:37:25 +0600

    add all product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,19 +1,22 @@
+import { BsBasket } from "react-icons/bs";
 
 
 const Product = ({ product }) => {
+
+    const { img, title, price } = product;
     return (
         <div>
             <div className="card bg-base-100 shadow-sm border">
                 <figure>
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        src={img}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="card-title">{title}</h2>
+                    <div className="card-actions">
+                        <p className="text-xl font-medium">${price}</p>
+                        <button className="btn bg-[#5AAC38] text-xl text-white"><BsBasket /></button>
                     </div>
                 </div>
             </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,18 +5,21 @@ import Product from "../Product/Product";
 const Products = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data =>setProducts(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div className="container mx-auto mt-12"> 
+        <div className="container mx-auto mt-12 mb-12">
             <h1 className="text-2xl font-medium text-center mb-14">All Product</h1>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
                 {
-                    products.map((product, idx) =><Product key={idx} product = {product}></Product>)
+                    products.map((product, idx) => <Product key={idx} product={product}></Product>)
                 }
+            </div>
+            <div className="text-center mt-8">
+                <button className="btn btn-outline border-[#5AAC38] text-lg text-[#5AAC38]">Load More</button>
             </div>
         </div>
     );
